5dde67e6267617c56382030fbbde849e2efb9d38
James Clark <jamesclark122006@gmail.com>
2022-04-23 22:10:59 -0500

    Added methods for GetSiteUrl, IsTestSite, GetControlReason, and added visible login with no RM

--- Selenium Script/Program.cs
+++ Selenium Script/Program.cs
@@ -27,15 +27,13 @@ namespace Selenium_Script
         public static void ControlPlus(IWebElement element, string key)
         {
             element.SendKeys(Keys.LeftControl + key); //passes in the element to be targeted, then pass in the key to use with control
-        }       
-        public static void LoginToSite()
+        }  
+        public static string GetSiteUrl()
         {
             Console.WriteLine("Enter Site URL to login to:");
             string siteUrl = Console.ReadLine().Trim();
             bool hasLogin = siteUrl.Contains("login");
             bool hasHttps = siteUrl.Contains("http");
-            bool testSite = false;
-            string controlReason = "";
 
             IDictionary<string, string> shorthandSites = new Dictionary<string, string>();
             shorthandSites.Add("kirk", "https://kirk.interactgo.com/login?local=True");
@@ -57,7 +55,7 @@ namespace Selenium_Script
                     //https was not found on the user input, add it.
                     siteUrl = "https://" + siteUrl;
                 }
-                
+
             }
             else
             {
@@ -65,21 +63,10 @@ namespace Selenium_Script
                 shorthandSites.TryGetValue(siteUrl, out siteUrl);
             }
 
-
-
-            
-
-            string[] testSites = { "kirk", "picard" };
-            if (testSites.Any(siteUrl.Contains))
-            {
-                testSite = true;
-            }
-            else
-            {
-                Console.WriteLine("Ticket Number/Reason: ");
-                controlReason = Console.ReadLine();
-            }
-
+            return siteUrl;
+        }
+        public static Cookie GetLoginCookie (string siteUrl, bool testSite, string controlReason)
+        {
             Console.WriteLine("Launching browser... ");
             var timer = new Stopwatch();
             timer.Start();
@@ -93,7 +80,7 @@ namespace Selenium_Script
             headlessOptions.AddArguments("--start-maximized", "--blink-settings=imagesEnabled=false", "--headless");
             //options.AddArguments(@"user-data-dir=C:\Users\James Clark\AppData\Local\Google\Chrome\User Data\Selenium");
             var chromeDriverService = ChromeDriverService.CreateDefaultService();
-            chromeDriverService.HideCommandPromptWindow = true; 
+            chromeDriverService.HideCommandPromptWindow = true;
 
 
             IWebDriver headlessDriver = new ChromeDriver(chromeDriverService, headlessOptions);
@@ -107,7 +94,7 @@ namespace Selenium_Script
             //Elements used for site main login page
             IWebElement siteUser = headlessDriver.FindElement(By.Id("Username"));
             IWebElement sitePass = headlessDriver.FindElement(By.Id("Password"));
-            
+
             IWebElement siteLoginBtn = headlessDriver.FindElement(By.Id("loginbtn"));
 
             try
@@ -118,7 +105,9 @@ namespace Selenium_Script
             {
                 Console.WriteLine("No 'Remember Me' checkbox found, cannot continue.");
                 headlessDriver.Quit();
-                return;
+                VisibleLoginToSite(siteUrl, testSite, controlReason);
+                Cookie nullCookie = null;
+                return nullCookie;
             }
 
             //Interactions with site main login page elements           
@@ -197,9 +186,9 @@ namespace Selenium_Script
             elapsedTime = timer.Elapsed;
             Console.WriteLine("Logging into " + siteUrl + "... " + elapsedTime.ToString(@"m\:ss\.fff"));
             challengeSubmit.Click();
-            
+
             string loggedInURL = headlessDriver.Url;
-            
+
 
             timer.Stop();
             elapsedTime = timer.Elapsed;
@@ -208,25 +197,190 @@ namespace Selenium_Script
 
             Cookie cookie = headlessDriver.Manage().Cookies.GetCookieNamed(".AspNet.ApplicationCookie");
 
-            //Create visible web driver (still uses chromeDriverService from headless driver)
+            return cookie;
+        }
+        public static bool IsTestSite(string siteUrl)
+        {
+            string[] testSites = { "kirk", "picard" };
+
+            if (testSites.Any(siteUrl.Contains))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static string GetControlReason()
+        {
+            string controlReason = "";
+            Console.WriteLine("Ticket Number/Reason: ");
+            controlReason = Console.ReadLine();
+            return controlReason;
+        }
+        public static string GetBaseUrl(string siteUrl)
+        {
+            string baseUrl = siteUrl.Remove(siteUrl.LastIndexOf("/") + 1);
+            return baseUrl;
+        }
+        public static void LoginToSite()
+        {
+            string siteUrl = GetSiteUrl();
+            string baseUrl = GetBaseUrl(siteUrl);
+            bool testSite = IsTestSite(siteUrl);
+            string controlReason = "";
+
+            if (testSite == false)
+            {
+                controlReason = GetControlReason();
+            }
+
+            Cookie cookie = GetLoginCookie(siteUrl, testSite, controlReason);
+
+            if (cookie == null)
+            {
+                return;
+            }
+
+            //Create visible web driver
             ChromeOptions siteOptions = new ChromeOptions();
             siteOptions.AddArgument("--start-maximized");
+            var chromeDriverService = ChromeDriverService.CreateDefaultService();
+            chromeDriverService.HideCommandPromptWindow = true;
             IWebDriver siteDriver = new ChromeDriver(chromeDriverService, siteOptions);
 
             siteDriver.Navigate().GoToUrl(siteUrl);
 
             siteDriver.Manage().Cookies.AddCookie(cookie);
 
-            siteDriver.Navigate().GoToUrl(loggedInURL);
+            siteDriver.Navigate().GoToUrl(baseUrl);
 
             //Open tabs
             IJavaScriptExecutor jsSite = (IJavaScriptExecutor)siteDriver;
-            jsSite.ExecuteScript("window.open('" + loggedInURL + "support')");
-            jsSite.ExecuteScript("window.open('" + loggedInURL + "Interact/Pages/Admin/Default.aspx?section=-1')");
-            jsSite.ExecuteScript("window.open('" + loggedInURL + "Interact/Pages/Admin/People/Staff/Default.aspx?section=106')");
-            jsSite.ExecuteScript("window.open('" + loggedInURL + "InteractV7/UMI/List')");
+            jsSite.ExecuteScript("window.open('" + baseUrl + "support')");
+            jsSite.ExecuteScript("window.open('" + baseUrl + "Interact/Pages/Admin/Default.aspx?section=-1')");
+            jsSite.ExecuteScript("window.open('" + baseUrl + "Interact/Pages/Admin/People/Staff/Default.aspx?section=106')");
+            jsSite.ExecuteScript("window.open('" + baseUrl + "InteractV7/UMI/List')");
             siteDriver.SwitchTo().Window(siteDriver.WindowHandles.First());
 
+
+        }
+        public static void VisibleLoginToSite(string siteUrl, bool testSite, string controlReason)
+        {
+            Console.WriteLine("Launching browser... ");
+            var timer = new Stopwatch();
+            timer.Start();
+
+            //want to add a dictionary of test sites that you'd be able to edit from a yaml file 
+            //Add a way that you can add /support, application settings or something similar to end
+            //Open multiple tabs with support and application settings
+            //string testSite = "N";
+
+            ChromeOptions Options = new ChromeOptions();
+            Options.AddArgument("--start-maximized");
+            //options.AddArguments(@"user-data-dir=C:\Users\James Clark\AppData\Local\Google\Chrome\User Data\Selenium");
+            var chromeDriverService = ChromeDriverService.CreateDefaultService();
+            chromeDriverService.HideCommandPromptWindow = true;
+
+
+            IWebDriver Driver = new ChromeDriver(chromeDriverService, Options);
+
+            Driver.Navigate().GoToUrl(siteUrl);
+            //headlessDriver.Navigate().GoToUrl("https://thehub.integralads.com/login?local=True");
+            //headlessDriver.Navigate().GoToUrl("https://kirk.interactgo.com/login?local=True");
+            TimeSpan elapsedTime = timer.Elapsed;
+            Console.WriteLine("Loading " + siteUrl + "... " + elapsedTime.ToString(@"m\:ss\.fff"));
+
+            //Elements used for site main login page
+            IWebElement siteUser = Driver.FindElement(By.Id("Username"));
+            IWebElement sitePass = Driver.FindElement(By.Id("Password"));
+
+            IWebElement siteLoginBtn = Driver.FindElement(By.Id("loginbtn"));
+
+            //Interactions with site main login page elements           
+            siteUser.SendKeys("admin");
+            sitePass.SendKeys("bantam");
+            siteLoginBtn.Click();
+            elapsedTime = timer.Elapsed;
+            Console.WriteLine("Grabbing challenge code... " + elapsedTime.ToString(@"m\:ss\.fff"));
+
+            //Challenge page Elements
+            IWebElement challengeBox = Driver.FindElement(By.Id("Challenge"));
+            IWebElement challengeFinal = Driver.FindElement(By.Id("Code"));
+            IWebElement challengeSubmit = Driver.FindElement(By.Id("submitchallenge"));
+            challengeBox.Click();
+            //Select all then copy the challenge code
+            ControlPlus(challengeBox, "a");
+
+            ControlPlus(challengeBox, "c");
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+
+            js.ExecuteScript("new_tab = window.open('https://control.interactgo.com/Account/Login')");
+            Driver.SwitchTo().Window(Driver.WindowHandles.Last());
+
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+
+            IWebElement controlUser = Driver.FindElement(By.Id("Email"));
+            IWebElement controlPassword = Driver.FindElement(By.Id("Password"));
+            IWebElement controlLoginBtn = Driver.FindElement(By.CssSelector(".btn-primary"));
+
+            //Control Site Main Interactions
+            controlUser.SendKeys("james.clark@interact-intranet.com");
+            controlPassword.SendKeys("R3trieval.br!nk.0verst@te");
+            controlLoginBtn.Click();
+            elapsedTime = timer.Elapsed;
+            Console.WriteLine("Logging into Control... " + elapsedTime.ToString(@"m\:ss\.fff"));
+
+            //Control Challenge Page Elements
+            IWebElement controlCodeBox = Driver.FindElement(By.CssSelector(".challenge-code"));
+            IWebElement controlCodeReason = Driver.FindElement(By.CssSelector(".challenge-reason"));
+            IWebElement controlCodeSubmit = Driver.FindElement(By.CssSelector(".challenge-submit"));
+
+            //Control Challenge Page Interactions
+
+            //Paste challenge code
+            ControlPlus(controlCodeBox, "v");
+
+            if (testSite == false)
+            {
+                wait.Until(ExpectedConditions.ElementToBeClickable(controlCodeReason));
+                controlCodeReason.SendKeys("ticket: " + controlReason);
+                controlCodeSubmit.Click();
+                Thread.Sleep(1500);
+            }
+            else
+            {
+                Thread.Sleep(1500); //wait 1.5 seconds for site to load code, need to fix this to be more reliable
+            }
+
+            //Select all in challenge code box
+            ControlPlus(controlCodeBox, "a");
+
+            //Copy the final code and close the Control window
+            ControlPlus(controlCodeBox, "c");
+
+            Driver.SwitchTo().Window(Driver.WindowHandles.First());
+            js.ExecuteScript("new_tab.close()");
+
+            elapsedTime = timer.Elapsed;
+            Console.WriteLine("Copied Control code..." + elapsedTime.ToString(@"m\:ss\.fff"));
+
+            //Move back to Main Site Challenge Page and paste in final code
+            ControlPlus(challengeFinal, "v");
+
+            elapsedTime = timer.Elapsed;
+            Console.WriteLine("Logging into " + siteUrl + "... " + elapsedTime.ToString(@"m\:ss\.fff"));
+            challengeSubmit.Click();
+
+            string loggedInURL = Driver.Url;
+
+
+            timer.Stop();
+            elapsedTime = timer.Elapsed;
+            Console.WriteLine("Finished logging into " + loggedInURL);
+            Console.WriteLine("Total login time: " + elapsedTime.ToString(@"m\:ss\.fff") + "\n");
 
         }
     }
